ec7bbceb67829e3a18496f80f6e235dfd9b91b94
SARA BLEIER <sarabbleier@gmail.com>
2025-02-05 18:34:04 -0500

    try #3

--- src/main/java/bleier/physics/Force.java
+++ src/main/java/bleier/physics/Force.java
@@ -4,38 +4,42 @@ public class Force {
     private double degrees;
     private double magnitude;
     private double radians;
-    private double X;
-    private double Y;
+    private double x;
+    private double y;
 
     public Force() {
         degrees = 0;
         magnitude = 0;
         radians = 0;
-        X = 0;
-        Y = 0;
+        x = 0;
+        y = 0;
     }
+
     public Force(int degrees, int magnitude) {
         this.degrees = degrees;
         this.magnitude = magnitude;
         radians = Math.toRadians(degrees);
-        X= magnitude * Math.cos(radians);
-        Y = magnitude * Math.sin(radians);
+        x = magnitude * Math.cos(radians);
+        y = magnitude * Math.sin(radians);
     }
 
     public Force add(Force force) {
         Force solution = new Force();
-        solution.X = this.X + force.X;
-        solution.Y = this.Y + force.Y;
-        solution.magnitude = Math.sqrt(solution.X * solution.X + solution.Y * solution.Y);
-        solution.degrees = Math.toDegrees(Math.atan(solution.Y / solution.X));
+        solution.x = this.x + force.x;
+        solution.y = this.y + force.y;
+        solution.magnitude = Math.sqrt(solution.x * solution.x
+                + solution.y * solution.y);
+        solution.degrees = Math.toDegrees(Math.atan(solution.y / solution.x));
         return solution;
     }
 
     @Override
     public String toString() {
         return "Force{" +
-                "magnitude=" + magnitude +
-                ", degrees=" + degrees +
-                '}';
+                "magnitude="
+                + magnitude
+                + ", degrees="
+                + degrees
+                + '}';
     }
 }
